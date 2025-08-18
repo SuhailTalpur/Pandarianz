da218034026fff14d158d82321296d0bbd29b469
SuhailTalpur <suhailbaloch2000@gmail.com>
2025-08-18 22:05:10 +0500

    third comit

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -4,9 +4,20 @@ function Footer() {
   return (
     <footer className="bg-[#1C1C2A] text-white py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} Made with ❤️ by Xytra Lab</p>
+        <p>
+          &copy; {new Date().getFullYear()} Made with ❤️ by{" "}
+          <a
+            href="https://x.com/XytraLab"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            Xytra Lab
+          </a>
+        </p>
       </div>
     </footer>
+
   );
 }
 
